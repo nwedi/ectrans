29b1ae9ef5ef399caf53cdadc2f06761decacbed
Sam Hatfield <samuel.hatfield@ecmwf.int>
2026-01-26 17:12:33 +0000

    Try using arch files to pass flags

---
+++ .github/arch/amdflang-22.2.0.cmake
@@ -0,0 +1,7 @@
+set( CMAKE_CXX_FLAGS "-fPIC" )
+set( CMAKE_C_FLAGS "-fPIC" )
+set( CMAKE_Fortran_FLAGS "-fPIC -fno-stack-arrays")
+
+set( OpenMP_CXX_FLAGS "-fopenmp" )
+set( OpenMP_C_FLAGS "-fopenmp" )
+set( OpenMP_Fortran_FLAGS "-fopenmp" )
